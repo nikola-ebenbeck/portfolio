4520a185d15628373dfa62d33fd649c435207342
Nikola Ebenbeck <nikola@ebenbeck.de>
2026-03-13 18:51:13 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,103 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
+    "Simulieren statt Spekulieren? - Chancen und Grenzen von Simulationsstudien in der Inklusionsforschung zu Bildungssystemen <br />39. Jahrestagung Inklusionsforschung in Bremen",
+    53.10663062483398, 
+    8.855342072109599
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Feste Sonderpädagog:innen an jeder Grundschule? – Eine Simulationsstudie zu den Folgen von Sonderschulschließungen auf die personellen Ressourcen von Grundschulen in inklusiven Bildungssystemen <br />33. Jahrestagung der DGfE Kommission Grundschulforschung und Pädagogik der Primarstufe, Vorarlberg, Österreich",
+    47.23408449770347, 
+    9.590438432545604
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Was wäre, wenn Bayern inklusive Schulen statt Förderschulen hätte? <br />11. Tagung der Gesellschaft für empirische Bildungsforschung, Potsdam ",
+    52.40116646799091, 
+    13.012318736551977
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "Trends in der datengestützten Förderdiagnostik. Der Mehrwert digitaler und adaptiver Screening- und Lernverlaufsinstrumente. <br />Jubiläumskongress des Bundesverbandes Legasthenie und Dyskalkulie e.V., Würzburg",
+    49.78824975982073, 
+    9.934908780695823
+  ],
+  [
+    "Digitale alltagsintegrierte Förderdiagnostik im Lesen mit Levumi <br />Dies Academicus, Leipzig",
+    51.338687043828116, 
+    12.378884159805486
+  ],
+  [
+    "Developing a Digital, Adaptive, Inclusive Reading Screening. Evaluating Test Modification’s Impact on Students with and without Learning Disabilities’ Performance <br />European Conference on Educational Research, Glasgow, UK",
+    55.87233192752838, 
+    -4.290241060843519
+  ],
+  [
+    "Digitale alltagsintegrierte Förderdiagnostik im Lesen. Zwischenergebnisse der Teilprojekte <br />BMBF Jahrestagung Förderbezogene Diagnostik in der inklusiven Bildung, Frankfurt",
+    50.127149922656606, 
+    8.668652473828175
+  ],
+  [
+    "Computerized Adaptive Testing in Inclusive Education 
+    <br />Universität Regensburg",
+    48.996923546076594, 
+    12.094567594917997
+  ],
+  [
+    "Adaptives Testen in der Sonderpädagogik. Simulation computerbasierter adaptiver Tests basierend auf Speed-Test Daten <br />Frühjahrstagung der Arbeitsgruppe für empirische sonderpädagogische Forschung, Hannover",
+    52.38239647823763, 
+    9.718187779702266
+  ],
+  [
+    "Open Science konkret. Wie nutze ich Plattformen wie OSF.io und GitHub? <br />Nachwuchskongress des 13. Kongress der Gesellschaft für Empirische Bildungsforschung, München",
+    48.148870856731314, 
+    11.56862830594193
+  ],
+  [
+    "Open Educational Resources. Workshop für Prä-Docs. <br />Open Science Winter School Graz, Österreich",
+    47.082069822882424, 
+    15.438985900320521
+  ],
+   [
+    "Programmieren mit R und tidyverse für Fortgeschrittene <br />Prä-Herbsttagung der AESF, München",
+    48.156116357152435, 
+    11.583277815406902
+  ],
+   [
+    "Lernverlaufsdiagnostik mit LEVUMI im Förderschwerpunkt Geistige Entwicklung <br />Fachtag Digitale Bildung in Förderschulen, München",
+    48.156116357152435, 
+    11.583277815406902
+  ],
+  [
+    "Lernverlaufsdiagnostik mit LEVUMI <br />Lehrkräftefortbildung für Grundschulen",
+    48.43166303687418, 
+    11.601296846916894
+  ],
+  [
+    "Lernverlaufsdiagnostik mit LEVUMI <br />Lehrkräftefortbildung für Grundschulen",
+    48.46333772397759, 
+    11.928589538041008
+  ],
+  [
+    "Lernverlaufsdiagnostik mit LEVUMI <br />Lehrkräftefortbildung für Grundschulen",
+    48.51360864605717, 
+    11.907854499828277
+  ],
+  [
+    "Lernverlaufsdiagnostik mit LEVUMI <br />Lehrkräftefortbildung für Grundschulen",
+    48.415089139351394, 
+    11.753562543135398
+  ],
+  [
+    "Lernverlaufsdiagnostik mit LEVUMI <br />Lehrkräftefortbildung für Grundschulen",
+    49.94234837908056, 
+    11.577055209593825
+  ],
+  [
+    "Lernverlaufsdiagnostik mit LEVUMI <br />Lehrkräftefortbildung für Grundschulen",
+    48.245600090456136, 
+    11.659603833866184
+  ],
+  [
+    "Lernverlaufsdiagnostik mit LEVUMI <br />Lehrkräftefortbildung für Grundschulen",
+    48.80125874513168, 
+    12.340774975297489
   ]
 ];
